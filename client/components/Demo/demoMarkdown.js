1995e8bb7ec79f652d77d643d26d5848c154a6bf
deadlybutter <thedeadlybutter@gmail.com>
2017-10-28 19:01:41 -0400

    Adding more type styling

--- client/components/Demo/demoMarkdown.js
+++ client/components/Demo/demoMarkdown.js
@@ -1,22 +1,22 @@
 const markdown = `
 
 # Header 1
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium tempus odio et convallis.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. __Vivamus pretium__ tempus odio et convallis.
 
 ## Header 2
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium tempus odio et convallis.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. __Vivamus pretium__ tempus odio et convallis.
 
 ### Header 3
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium tempus odio et convallis.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. __Vivamus pretium__ tempus odio et convallis.
 
 #### Header 4
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium tempus odio et convallis.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. __Vivamus pretium__ tempus odio et convallis.
 
 ##### Header 5
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium tempus odio et convallis.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. __Vivamus pretium__ tempus odio et convallis.
 
 ###### Header 6
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium tempus odio et convallis.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. __Vivamus pretium__ tempus odio et convallis.
 `;
 
 export default markdown;
