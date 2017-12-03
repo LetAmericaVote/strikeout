432c6ca183f3707ca6147eb613456ffff4e0c863
deadlybutter <thedeadlybutter@gmail.com>
2017-12-02 23:10:59 -0500

    using gzip package

--- server/web.js
+++ server/web.js
@@ -1,17 +1,11 @@
+const gzip = require('itsjoekent-express-gzip');
 const express = require('express');
 const app = express();
 
 app.use('/public', express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
-  function gzip(req, res, next) {
-    req.url = req.url + '.gz';
-    res.set('Content-Encoding', 'gzip');
-    next();
-  }
-
-  app.get('/public/dist/*.js', gzip);
-  app.get('/public/dist/*.css', gzip);
+  gzip.commonRoutes(app, '/public/dist/');
 }
 
 app.get('*', (req, res, next) => {
