758b0d9a6f10e5d1fceb948070a76ff9e31a17d9
deadlybutter <thedeadlybutter@gmail.com>
2017-12-03 14:11:41 -0500

    switching to server builder helper

--- server/web.js
+++ server/web.js
@@ -1,18 +1,10 @@
-const gzip = require('itsjoekent-express-gzip');
-const express = require('express');
-const app = express();
+const path = require('path');
+const server = require('itsjoekent-server');
 
-app.use('/public', express.static('public'));
-
-if (process.env.NODE_ENV === 'production') {
-  gzip.commonRoutes(app, '/public/dist/');
-}
-
-app.get('*', (req, res, next) => {
-  res.sendFile(`${__dirname}/template.html`);
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.info(`Listening on ${PORT}`);
-});
+const app = server
+  .makeApp()
+  .public('/public', path.join(__dirname, '../public'))
+  .gzip('/public/dist/', true)
+  .spa(`${__dirname}/template.html`)
+  .listen()
+  .app;
